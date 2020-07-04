f8448d5a38701499ad770f0531f1246e87bf2b2b
sakthis2087 <64739590+sakthis2087@users.noreply.github.com>
2020-07-05 00:39:36 +0530

    portfolio

---
+++ page.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<string:page_name>")
+def home(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('db.txt',mode='a') as db:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = db.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('db.csv',mode='a',newline='') as db2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == "POST":
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return "Did not save to database"
+    else:
+        return('ERROR')
